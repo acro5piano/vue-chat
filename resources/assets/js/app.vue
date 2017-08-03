3ed01d420d5b2cc5c90a8a670c6b959cd8f751e7
yasuhiro yokota <yktyshr@gmail.com>
2017-08-03 15:46:23 +0900

    1st design

--- resources/assets/js/app.vue
+++ resources/assets/js/app.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
+
+
+  <div class="message-wrap">
+
+  <header class="global-header">
+    <h1>なぜにゃん</h1>
+  </header>
     <div v-for="message in messages">
       <div class="message" :class="{'message-sent': message.fromMe, 'message-received': ! message.fromMe}">
-        <div class="message-sender">
-          {{ message.sender }}
+          <span class="message-sender">
+            {{ message.sender }}
+          </span>
+
+        <div class="message-bubble">
+
+
+          <div class="message-content">
+            {{ message.content }}
+          </div>
+
         </div>
-        {{ message.content }}
       </div>
     </div>
-    <input @keydown.enter="sendMessage" v-model="newMessageContent">
-    <button @click="sendMessage">送信</button>
+    <div class="input-area">
+        <input @keydown.enter="sendMessage" v-model="newMessageContent"></textarea>
+        <button @click="sendMessage">送信</button>
+          
+    </div>
   </div>
 </template>
 
@@ -50,16 +67,5 @@
 </script>
 
 <style>
-.message {
-  padding: 20px;
-  margin-bottom: 30px;
-  border: solid 1px #ccc;
-  border-radius: 5px;
-}
-.message-sent {
-  text-align: right;
-}
-.message-sender {
-  color: #ccc;
-}
+
 </style>
